c649978fb1d6a064cfb52ac146d646ab7f955368
Saulo MacChelsom <2033.xyz@gmail.com>
2023-05-17 21:41:38 -0300

    list

--- src/app/pages/admin/my_todo/components/filter/filter.component.ts
+++ src/app/pages/admin/my_todo/components/filter/filter.component.ts
@@ -4,6 +4,11 @@ import { debounceTime, takeUntil } from 'rxjs/operators';
 import { Filter } from '../../models/filter';
 import { Subject } from 'rxjs';
 
+interface FilterOption {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
@@ -11,6 +16,13 @@ import { Subject } from 'rxjs';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FilterComponent implements OnInit, OnDestroy {
+  
+  public foods: FilterOption[] = [
+    {value: 'isPrivate', viewValue: 'Private'},
+    {value: 'isBusiness', viewValue: 'Business'},
+  ];
+
+
   @Input()
   set filter(filter: Filter) {
     this.formGroup.setValue(filter, { emitEvent: false });
@@ -21,7 +33,7 @@ export class FilterComponent implements OnInit, OnDestroy {
 
   formGroup: FormGroup = new FormGroup({
     search: new FormControl(),
-    category: new FormGroup({
+    category: new FormControl({
       isBusiness: new FormControl(),
       isPrivate: new FormControl(),
     }),
@@ -33,34 +45,39 @@ export class FilterComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     // Debounce just the text input
-    this.formGroup
-      .get('search')
-      .valueChanges.pipe(takeUntil(this.unsubscribe$), debounceTime(350))
-      .subscribe((value) => {
-        this.filterUpdate.emit({
-          ...this.formGroup.value,
-          search: value,
-        });
+    this.formGroup.get('search').valueChanges.pipe(takeUntil(this.unsubscribe$), debounceTime(350)).subscribe((value) => {
+      this.filterUpdate.emit({
+        ...this.formGroup.value,
+        search: value,
       });
+    });
 
-    this.formGroup
-      .get('category')
-      .valueChanges.pipe(takeUntil(this.unsubscribe$))
-      .subscribe((value) => {
-        this.filterUpdate.emit({
-          ...this.formGroup.value,
-          category: value,
-        });
+    this.formGroup.get('category').valueChanges.pipe(takeUntil(this.unsubscribe$)).subscribe((value) => {
+      console.log(value)
+        let category_value = { isBusiness:false, isPrivate:false}
+        if(value == 'isBusiness'){
+          category_value =  { isBusiness:true, isPrivate:false}
+        } if (value == 'isPrivate'){
+          category_value =  { isBusiness:false, isPrivate:true}
+        }
+
+      this.filterUpdate.emit({
+        ...this.formGroup.value,
+        category: category_value
       });
+    });
   }
 
-  public get search(){ 
-    return this.formGroup.get('search') 
+  public get search(): FormControl { 
+    return this.formGroup.get('search') as FormControl;
+  }
+
+  public  get category(): FormControl {
+    return this.formGroup.get('category') as FormControl;
   }
 
   public cleanUser(){
-    this.formGroup
-      .get('search').reset()
+    this.formGroup.get('search').reset()
   }
 
   ngOnDestroy(): void {
